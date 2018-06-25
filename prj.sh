b6f7140386bbe1ec89217496aaab0341d6fe504c
JackerDune <JackerDune@gmail.com>
2018-06-25 20:01:16 +0800

    增加gtags相关的文件索引创建方法

--- prj.sh
+++ prj.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-rm -f cscope.file
+rm -f cscope.files
 rm -f cscope.out
-find `pwd` -regex '.*\.h\|.*\.c\|.*\.cpp\|.*\.hh\|.*\.S\|.*\.s\|.*\.html\|.*\.css\|.*\.php\|.*\.java\|.*\.py\|.*\.js' > cscope.file
-cscope -bkRi cscope.file
+find `pwd` -regex '.*\.h\|.*\.c\|.*\.cpp\|.*\.hh\|.*\.S\|.*\.s\|.*\.html\|.*\.css\|.*\.php\|.*\.java\|.*\.py\|.*\.js' > cscope.files
+cp cscope.files gtags.files
+cscope -bkRi cscope.files
+gtags -v
+
+#find `pwd` -regex '.*\.h\|.*\.c\|.*\.cpp\|.*\.hh\|.*\.S\|.*\.s\|.*\.html\|.*\.css\|.*\.php\|.*\.java\|.*\.py\|.*\.js' |xargs etags
+
+
+#etags etags
